227f23a86c2e8e28979a1202f5b36b02e1668d0b
unknown <alison.souza.707@ufrn.edu.br>
2025-06-10 00:50:38 -0300

    feat: Adicionando detecção de idioma ao carregar a página.

--- app.vue
+++ app.vue
@@ -3,7 +3,15 @@
 </template>
 
 <script lang="ts" setup>
+const { locales, setLocale, getLocaleCookie } = useI18n()
+
+//Execute langauge detection
+onMounted(() => {
+  if (navigator.language.includes("pt") || navigator.language.includes("es")) setLocale("br")
+  else setLocale("en")
+})
 </script>
+
 
 <style lang="scss">
 html, body {
