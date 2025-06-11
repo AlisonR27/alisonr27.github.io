b5fcc40ccd35a004fec402e7d511a9a2a214fa0d
unknown <alison.souza.707@ufrn.edu.br>
2025-06-11 17:56:53 -0300

    feat: Criando componente Genérico de Tag

---
+++ components/generics/tag.vue
@@ -0,0 +1,24 @@
+<template>
+  <span class="tag" :class="{ 'star' : isFavorite }">
+    {{ label }}
+  </span>
+</template>
+
+<script lang="ts" setup>
+  const props = defineProps(['label'])
+
+  const favorites = [
+      "vue.js",
+      "laravel",
+      "php",
+      "scss"
+  ] as String[]
+
+  function isFavorite(label: String) {
+    return favorites.includes(label)
+  }
+</script>
+
+<style>
+
+</style>
